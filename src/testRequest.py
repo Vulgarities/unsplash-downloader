65435354b64392095d573ba2f993cf0c0cfe21e3
yanghaodong <yanghaodong@meituan.com>
2021-12-08 19:48:56 +0800

    123

--- src/testRequest.py
+++ src/testRequest.py
@@ -5,7 +5,7 @@ class testClass:
         self.name=name
         self.age=age
     def print1(self):
-        print("我的名字是%s,我的年龄是%d"%(self.name,self.age))
+        print("?%s,我的年龄是%d"%(self.name,self.age))
     def testDownload(self):
         headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3861.400 QQBrowser/10.7.4313.400'}
         url='https://img.ivsky.com/img/tupian/t/201008/05/bianxingjingang-001.jpg'
